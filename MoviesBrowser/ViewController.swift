bd287d26e8bba961aee88568d2cc107dcfe274d9
Aleksander Popko <aleksander.popko+1@gmail.com>
2023-12-06 12:24:01 +0100

    Add initial Xcode project

---
+++ MoviesBrowser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoviesBrowser
+//
+//  Created by Aleksander Popko on 06/12/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
