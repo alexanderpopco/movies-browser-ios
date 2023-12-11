4d92a1ba239cbb24ce1fc29d4bd2d90cd8759d31
Aleksander Popko <aleksander.popko+1@gmail.com>
2023-12-11 21:18:17 +0100

    Fix bug with saving favourtie movies

--- MoviesBrowser/Persistence/Storage.swift
+++ MoviesBrowser/Persistence/Storage.swift
@@ -15,18 +15,18 @@ struct Storage {
     
     private let userDefaults = UserDefaults()
     
-    func favourtieMoviesIds() -> [Int]? {
-        return userDefaults.array(forKey: StorageKeys.favouriteMoviesIds) as? [Int]
+    func favourtieMoviesIds() -> [Int] {
+        return userDefaults.array(forKey: StorageKeys.favouriteMoviesIds) as? [Int] ?? [Int]()
     }
     
     func saveMovieAsFavourite(movieId: Int) {
         var movieIds = favourtieMoviesIds()
-        movieIds?.append(movieId)
+        movieIds.append(movieId)
         userDefaults.set(movieIds, forKey: StorageKeys.favouriteMoviesIds)
     }
     
     func removeMovieFromFavourites(movieId: Int) {
-        let filteredMovieIds = favourtieMoviesIds()?.filter { $0 != movieId }
+        let filteredMovieIds = favourtieMoviesIds().filter { $0 != movieId }
         userDefaults.set(filteredMovieIds, forKey: StorageKeys.favouriteMoviesIds)
     }
 }
